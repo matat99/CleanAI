d826bfadc768c15cc88552fae2e60d964976de27
matat99 <mrmatat99@gmail.com>
2023-06-15 20:10:22 +0100

    Add requirements.txt, /example and updated main.py
    Main.py now has a function, still very crude, that reads text from a pdf.
    For testing purposes the pdf location is hardcoded but I do not intend
    on keeping it that way.
    
    example directory stores a copy of Alice in Wonderland that I can use for
    testing and as an example for any users.
    
    requirements.txt will contain any required python libraries

--- code/main.py
+++ code/main.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 
-print("Hello World")
+import PyPDF2
+
+pdf = "../example/Alice_in_Wonderland.pdf"
+
+def text_extract(pdf):
+	# Open the pdf file
+	with open(pdf, "rb") as file:
+		# Create PDF reader object
+		reader = PyPDF2.PdfReader(file)
+
+		page = reader.pages[5]
+		
+		text = page.extract_text()
+
+		print(text)
+
+text_extract(pdf)
